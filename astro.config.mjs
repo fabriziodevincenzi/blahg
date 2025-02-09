c4606e50d65e08e7afa0643e2917f5a936329371
Fabrizio <102367490+fabriziodevincenzi@users.noreply.github.com>
2025-02-10 00:06:39 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fabriziodevincenzi.netlify.app",
-	base: "/",
-	integrations: [sitemap()],
-	markdown: {
-		shikiConfig: {
-			theme: "material-theme-darker",
-			langs: [],
-		},
-	},
+    site: "https://fabriziodevincenzi.netlify.app",
+    base: "/",
+    outDir: "./dist",  // Aggiungi questa riga
+    integrations: [sitemap()],
+    markdown: {
+        shikiConfig: {
+            theme: "material-theme-darker",
+            langs: [],
+        },
+    },
 });
